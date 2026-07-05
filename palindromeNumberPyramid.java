c429b407b4261eb942febe16443776c6827c5f69
ShivamBari2728 <shivambari2728@gmail.com>
2026-07-05 15:10:43 +0530

    implemented palindrome number pyramid

---
+++ palindromeNumberPyramid.java
@@ -0,0 +1,63 @@
+class palindromeNumberPyramid {
+    public static void main(String[] args) {
+
+        /* 
+        int n = 4;
+        for (int i = 1; i <= n; i++) { // for rows
+
+            for (int k = 1; k < i; k++) { // for asending numbers
+
+                System.out.print(k);
+            }
+
+            for (int j = i; j <= i; j--) { // for decending numbers
+                if (j > 0) {
+                    System.out.print(j);
+                } else {
+                    break;
+                }
+
+            }
+            System.out.println();
+
+
+        }
+
+        output of follwing code :
+        1
+        121
+        12321
+        1234321
+        */
+
+
+
+
+        int n = 4;
+        int space = n;
+        for (int i = 1; i <= n; i++) { // for rows
+
+            for (int s = 1; s< space ; s ++){
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k < i; k++) { // for asending numbers
+
+                System.out.print(k);
+            }
+
+            for (int j = i; j <= i; j--) { // for decending numbers
+                if (j > 0) {
+                    System.out.print(j);
+                } else {
+                    break;
+                }
+
+            }
+            System.out.println();
+            space -=1;
+
+
+        }
+    }
+}
