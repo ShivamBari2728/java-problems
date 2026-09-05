d3dd5d99b8206034cd1456ea62e8d43301720c48
ShivamBari2728 <shivambari2728@gmail.com>
2026-09-05 14:57:30 +0530

    added optimal solution for rotating array

--- rotateArray.java
+++ rotateArray.java
@@ -71,3 +71,36 @@ public class rotateArray {
         System.out.println(Arrays.toString(nums));
     }
 }
+
+// leet code best approtch 
+
+/*
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        if (nums.length == 0) {
+            return;
+        }
+
+        k = k % nums.length;
+
+        int n = nums.length;
+
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+
+        for (int i = n - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+        }
+
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
+
+*/
